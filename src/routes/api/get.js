6ab38e436d8105d57f2136c7e643650d6f294bb7
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2025-10-03 11:42:26 -0400

    Updated get/ fragments route to send back fragments object or ids

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,13 +1,33 @@
 // src/routes/api/get.js
 
-const { createSuccessResponse } = require("../../response");
+const hash = require("../../hash");
+const logger = require("../../logger");
+const { Fragment } = require("../../model/fragment");
+const { createSuccessResponse, createErrorResponse } = require("../../response");
 
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-  // TODO: this is just a placeholder. To get something working, return an empty array...
-  res.status(200).json(
-    createSuccessResponse({ fragments: [] })
-  );
+module.exports = async (req, res) => {
+  try {
+    const expand = req.query.expand == 1;
+    const hashedEmail = hash(req.user);
+    let fragments;
+
+    logger.info({ expand }, 'Fetching fragment list for a user');
+
+    try {
+      fragments = await Fragment.byUser(hashedEmail, expand);
+    } catch (err) {
+      logger.error({ err, user: hashedEmail, expand: expand }, 'Error fetching fragment list for a user');
+      return res.status(500).json(createErrorResponse(500, 'Unable to retrieve fragments'));
+    }
+
+    logger.debug({ user: hashedEmail, expand: expand, count: fragments.length }, 'Fragments retrieved');
+
+    res.status(200).json(createSuccessResponse({ fragments: fragments }));
+  } catch (err) {
+    logger.error({ err }, 'Get: Unexpected error in fetching fragments by user');
+    res.status(500).json(createErrorResponse(500, 'Internal Server Error'));
+  }
 };
